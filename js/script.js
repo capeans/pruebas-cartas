0b1eaf007636c96a2f629a15292552ca89e2ee27
capeans <42524823+capeans@users.noreply.github.com>
2025-10-04 11:43:04 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -69,7 +69,7 @@ document.addEventListener('DOMContentLoaded', () => {
         render(filtrados);
       };
 
-      render(productosMostrados);
+      aplicarFiltros();
 
       filtroNombre?.addEventListener('input', aplicarFiltros);
       filtroCategoria?.addEventListener('change', aplicarFiltros);
